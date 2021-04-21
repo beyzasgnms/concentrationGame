2aac7ce66a75f2682167d46511d2a9b9f256397f
beyzasgnms <beyzasgnms@gmail.com>
2021-04-22 01:05:04 +0300

    introAndMVC

---
+++ Concentration.swift
@@ -0,0 +1,8 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Beyza Sığınmış on 21.04.2021.
+//
+
+import Foundation
---
+++ Concentration/Card.swift
@@ -0,0 +1,8 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Beyza Sığınmış on 21.04.2021.
+//
+
+import Foundation
